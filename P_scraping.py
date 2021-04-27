ef8a9a9747501b10b57a68e039d27d47e2bae2a2
Prasad-adhi <68509452+Prasad-adhi@users.noreply.github.com>
2021-04-27 16:47:53 +0530

    Add files via upload

--- P_scraping.py
+++ P_scraping.py
@@ -2,36 +2,59 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from googlesearch import search
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import sys
 # link for extract html data
 
 def getdata(url):
-    r = requests.get(url)
+    r = requests.get(url,verify=False)
     return r.text
 
-h_tags=[]
+def get_my_key(obj):
+  return obj["similarity"]
 
-query = "someone fainted"
-URL=search(query)[1]  
+h_tag_data=[]
+h_tags=["h1","h2","h3","h4","h5","h6"]
+query = "What to Do During a Heart Attack"
+#print(search(query))
+URL=search(query)[2]  
 print(URL)
 #htmldata = getdata("https://www.geeksforgeeks.org/")
-htmldata = getdata(URL)
+htmldata = getdata("https://share.upmc.com/2015/07/what-to-do-if-someone-is-having-a-heart-attack/")
 soup = BeautifulSoup(htmldata, 'html.parser')
 data = ''
-for data in soup.find_all("h2"):
-    h_tags.append(data.get_text())
+for Htag in h_tags:
+    for data in soup.find_all(Htag):
+        h_tag_data.append({"tag":Htag,"text":data.get_text()})
 
+for i in range(len(h_tag_data)):
+    text=[h_tag_data[i]["text"],query]
+    cv=CountVectorizer()
+    count_matrix=cv.fit_transform(text)
+    h_tag_data[i]["similarity"]=cosine_similarity(count_matrix)[0][1]
 
-tag='h2'
-for i in h_tags:
-    if(i==" What to do when someone faints"):
-        while(True):
-            try:
-                text2 = soup.find(tag, text=i).find_next('p').text
-            except:
-                break
-            print(text2)
-            tag='p'
-            i=text2
+h_tag_data.sort(key=get_my_key,reverse=True)
+print(h_tag_data)
+tag=h_tag_data[0]["tag"]
+i=h_tag_data[0]["text"]
+while(True):
+    try:
+        print("searching for p tags")
+        text2 = soup.find(tag, text=i).find_next('p').text
+        tag='p'
+    except:
+        try:
+            print("searching for ul")
+            #text2 = soup.find(tag, text=i).find_next('ul').text
+            print("searching for li")
+            #ul_tag=soup.find(tag, text=i).find_next('ul')
+            text2 = list(soup.find(tag, text=i).find_next('ul').descendants)
+        except:
+            break
+    print(text2)
+    i=text2
+
 
 
 #for data in soup.find_all("li"):
